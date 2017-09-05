500134143b411ca70ab0510ec0f45c2856c1cf61
Martin Pavlov <martin@codific.eu>
2017-09-05 16:11:07 +0200

    allow list for secret backend

--- src/Services/Data.php
+++ src/Services/Data.php
@@ -46,4 +46,8 @@ class Data
         return $this->client->delete('/v1/' . $path);
     }
 
+    public function list($path)
+    {
+        return $this->client->listRequest('/v1/' . $path);
+    }
 }
